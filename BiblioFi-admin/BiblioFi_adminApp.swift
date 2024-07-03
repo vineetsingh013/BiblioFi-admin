63dfddf838ac1c9cba973bdb23d2d5c517822740
vineetsingh013 <vineet.chaudhary013@gmail.com>
2024-07-03 15:55:34 +0530

    Initial Commit

---
+++ BiblioFi-admin/BiblioFi_adminApp.swift
@@ -0,0 +1,27 @@
+//
+//  BiblioFi_adminApp.swift
+//  BiblioFi-admin
+//
+//  Created by Vineet Chaudhary on 03/07/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct BiblioFi_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
