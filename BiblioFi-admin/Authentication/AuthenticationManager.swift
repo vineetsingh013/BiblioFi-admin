bc67e2d34f1e4101e51ddf128b9c17e16054183b
Akshat Kamboj <akukamboj@gmail.com>
2024-07-04 22:14:36 +0530

    added sign in for admin

---
+++ BiblioFi-admin/Authentication/AuthenticationManager.swift
@@ -0,0 +1,41 @@
+//
+//  AuthenticationManager.swift
+//  BiblioFi-admin
+//
+//  Created by Akshat Kamboj on 04/07/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthDataResult {
+    let uid: String
+    let email: String?
+    let photoUrl: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+}
+
+final class AuthenticationManager {
+    
+    
+    static let shared = AuthenticationManager()
+    private init() { }
+    
+    
+    func createUser(email: String, password: String) async throws -> AuthDataResult{
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResult(user: authDataResult.user)
+    }
+    
+    func loginUser(email: String, password: String) async throws -> AuthDataResult {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResult(user: authDataResult.user)
+    }
+    
+    
+}
--- BiblioFi-admin/BiblioFi_adminApp.swift
+++ BiblioFi-admin/BiblioFi_adminApp.swift
@@ -9,19 +9,22 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      
     FirebaseApp.configure()
-
+    print("firebase configured!")
     return true
   }
 }
 
 @main
 struct BiblioFi_adminApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            LoginPage()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
